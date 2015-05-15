1267397166572a59d05ece22e78f03039496162c
Rayhan Vevaina <rayhan@Rayhans-iMac.local>
2015-05-15 16:38:31 -0400

    homepage + footer

--- js/app.js
+++ js/app.js
@@ -1,1 +1,37 @@
 $(document).foundation();
+
+//***************************
+//Header size fix
+//***************************
+
+var app = {};
+
+var $fullHeight = $(".full-height");
+var $headerContent = $(".header-content");
+
+app.fullHeight = function () {
+    var winHeight = $(window).height();
+
+    $fullHeight.css("height", winHeight);
+
+    var topMargin = (($fullHeight.height() - $(".top-bar").height()) - $headerContent.height()) / 2;
+    $headerContent.css({"margin-top": topMargin + "px"});
+};
+
+app.fullHeight();
+$(window).on("resize", app.fullHeight);
+
+$headerContent.mouseover(function(){
+    $(".container").addClass("active");
+});
+
+$headerContent.mouseout(function(){
+    $(".container").removeClass("active");
+});
+
+$('img.arrow').click(function () {
+    $('html, body').animate({
+        scrollTop: $("#main").offset().top
+    }, 800);
+});
+
