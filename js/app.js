48f330aa09cc061c1665823a0e10df23d1cc5201
Rayhan Vevaina <rayhan@Rayhans-iMac.local>
2015-05-29 11:42:34 -0400

    design changes

--- js/app.js
+++ js/app.js
@@ -7,6 +7,7 @@ $(document).foundation();
 var app = {};
 
 var $fullHeight = $(".full-height");
+var $adjustWidth = $(".full-width");
 var $headerContent = $(".header-content");
 
 app.fullHeight = function () {
@@ -21,6 +22,21 @@ app.fullHeight = function () {
 app.fullHeight();
 $(window).on("resize", app.fullHeight);
 
+app.fullWidth = function(){
+    var winWidth = $(window).width() /2.4;
+
+    $adjustWidth.css("height", winWidth);
+
+    console.log(winWidth);
+};
+
+app.fullWidth();
+$(window).on("resize", app.fullWidth);
+
+
+//***************************
+//Mouseout function
+//***************************
 $headerContent.mouseover(function(){
     $(".container").addClass("active");
 });
@@ -34,4 +50,70 @@ $('img.arrow').click(function () {
         scrollTop: $("#main").offset().top
     }, 800);
 });
+
+//***************************
+//Map Initialization
+//***************************
+
+function initialize() {
+    var mapCanvas = document.getElementById('map-canvas');
+    var myLatlng = new google.maps.LatLng(19.066444, 72.823092);
+    var styles = [
+        {
+            stylers: [
+                { hue: "#00ffe6" },
+                { saturation: -20 }
+            ]
+        },{
+            featureType: "road",
+            elementType: "geometry",
+            stylers: [
+                { lightness: 100 },
+                { visibility: "simplified" }
+            ]
+        },{
+            featureType: "road",
+            elementType: "labels",
+            stylers: [
+                { visibility: "off" }
+            ]
+        }
+    ];
+    var mapOptions = {
+        center: myLatlng,
+        zoom: 13,
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+    };
+
+    var map = new google.maps.Map(mapCanvas, mapOptions);
+
+    var contentString = '<div id="content">'+
+        '<div id="siteNotice">'+
+        '</div>'+
+        '<h3 id="firstHeading" class="firstHeading">Cama House</h3>'+
+        '<div id="bodyContent">'+
+        '<p>117 Pali Hill Bandra, Mumbai 400021</p>'+
+        '</div>'+
+        '</div>';
+
+    var infowindow = new google.maps.InfoWindow({
+        content: contentString
+    });
+
+    var marker = new google.maps.Marker({
+        position: myLatlng,
+        map: map,
+        title: 'Cama House'
+    });
+
+    google.maps.event.addListener(marker, 'click', function() {
+        infowindow.open(map,marker);
+    });
+
+    map.setOptions({styles: styles});
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
+
+
 
